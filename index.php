a51d9bb1b8fefb0d08d8d00511085262d9c922a5
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-09 08:30:05 -0600

    getting started not sure if I commited

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <!DOCTYPE html>
-<html>
+<!--parts of this code have been taken from examples by Rochelle Lewis and bootstrap.com -->
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<!-- set the IE rendering to help the nearly helpless IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<!-- sets view port width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!-- Fontawesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- cause IE needs more help -->
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- our css -->
+		<link rel="stylesheet" href="style.css" type="text/css"
 	</head>
 </html>
